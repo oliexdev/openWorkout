46010ca613ae48d136178e1a5f8fc8ddd803c308
senpl <urbanekmike@gmail.com>
2020-12-16 15:16:09 +0100

    new gradle and missing config

--- app/src/main/java/com/health/openworkout/BuildConfig.java
+++ app/src/main/java/com/health/openworkout/BuildConfig.java
@@ -15,32 +15,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+package com.health.openworkout;
 
-buildscript {
-    
-    repositories {
-        google()
-        jcenter()
-        
-    }
-    dependencies {
-        classpath 'com.android.tools.build:gradle:4.1.1'
-        classpath "androidx.navigation:navigation-safe-args-gradle-plugin:2.3.2"
+public class BuildConfig {
 
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        jcenter()
-        
-    }
-}
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
+    public static final boolean DEBUG = true;
 }
